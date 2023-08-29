10230de10215188cee58c17ddffab7193819e5dd
serethewind <osasereu@gmail.com>
2023-08-29 12:24:07 +0100

    Cicruit breaker, Retry Mechanism, Fallback pattern fully implemented with Resilience4j

--- exam-session/src/main/java/com/easytest/examsession/service/ExamService.java
+++ exam-session/src/main/java/com/easytest/examsession/service/ExamService.java
@@ -10,9 +10,12 @@ import com.easytest.examsession.entity.ExamEntity;
 import com.easytest.examsession.feignClient.FeignClientInterceptor;
 import com.easytest.examsession.feignClient.dto.QuestionResponseDto;
 import com.easytest.examsession.repository.ExamRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -22,15 +25,17 @@ import java.util.stream.Collectors;
 @Service
 @AllArgsConstructor
 @Slf4j
-public class ExamService implements ExamServiceInterface{
+public class ExamService implements ExamServiceInterface {
     private FeignClientInterceptor feignClient;
     private ExamRepository examRepository;
     private RabbitMQProducer rabbitMQProducer;
 
     @Override
+    @CircuitBreaker(name = "generateQuestions", fallbackMethod = "generateQuestionsFallbackMethod")
+    @Retry(name = "generateQuestions", fallbackMethod = "generateQuestionsFallbackMethod")
     public ResponseDto createQuizSession(ExamRequestDto examRequestDto) {
         //make a call at the question service to retrieve a category of questions with a predefined number of questions
-        List<Long> questionIdList= feignClient.generateSetOfQuestions(examRequestDto.getCategory(), examRequestDto.getNumberOfQuestions()).getBody();
+        List<Long> questionIdList = feignClient.generateSetOfQuestions(examRequestDto.getCategory(), examRequestDto.getNumberOfQuestions()).getBody();
         ExamEntity examEntity = ExamEntity.builder()
                 .title(examRequestDto.getTitle())
                 .category(examRequestDto.getCategory())
@@ -51,21 +56,32 @@ public class ExamService implements ExamServiceInterface{
                 .responseMessage("Successfully created")
                 .build();
     }
+    @Override
+    public ResponseEntity<ResponseDto> generateQuestionsFallbackMethod(ExamRequestDto examRequestDto, Throwable throwable) {
+        return new ResponseEntity<>(
+                ResponseDto.builder()
+                        .responseCode("")
+                        .responseMessage("Oops! Generating questions request taking too long.")
+                        .build(),
+                HttpStatus.REQUEST_TIMEOUT);
+    }
 
     @Override
+    @CircuitBreaker(name = "populateQuestions", fallbackMethod = "populateQuestionsFallbackMethod")
+    @Retry(name = "populateQuestions", fallbackMethod = "populateQuestionsFallbackMethod")
     public ExamResponseDto getQuestionsForExamSession(Long examId) {
-       ExamEntity examEntity = examRepository.findById(examId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        ExamEntity examEntity = examRepository.findById(examId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
 //        log.info("size of the list is " + String.valueOf(examEntity.getQuestionId().size()));
 
-       List<QuestionResponseDto> questionList = feignClient.getQuestionsBasedOnId(examEntity.getQuestionId()).getBody();
+        List<QuestionResponseDto> questionList = feignClient.getQuestionsBasedOnId(examEntity.getQuestionId()).getBody();
 
-       EmailDetails emailDetails = EmailDetails.builder()
-               .recipient("osasereu@gmail.com")
-               .message("Hello, you just viewed a new exam session. Do take note of the session id: " + examEntity.getId() + ". Exam session title generated " + examEntity.getTitle())
-               .subject("EXAM SESSION SUCCESSFULLY FETCHED")
-               .build();
-       rabbitMQProducer.sendViewQuizSessionEmailNotification(emailDetails);
+        EmailDetails emailDetails = EmailDetails.builder()
+                .recipient("osasereu@gmail.com")
+                .message("Hello, you just viewed a new exam session. Do take note of the session id: " + examEntity.getId() + ". Exam session title generated " + examEntity.getTitle())
+                .subject("EXAM SESSION SUCCESSFULLY FETCHED")
+                .build();
+        rabbitMQProducer.sendViewQuizSessionEmailNotification(emailDetails);
         log.info(emailDetails + "viewed session sent just now");
 //       return the exam i.e. the title and the actual questions associated with the question Ids stored using feign client
         return ExamResponseDto.builder()
@@ -73,22 +89,41 @@ public class ExamService implements ExamServiceInterface{
                 .title(examEntity.getTitle())
                 .listOfQuestions(questionList)
                 .build();
-
     }
-
     @Override
+    public ResponseEntity<ResponseDto> populateQuestionsFallbackMethod(Long examId, Throwable throwable) {
+        return new ResponseEntity<>(
+                ResponseDto.builder()
+                        .responseCode("")
+                        .responseMessage("Oops! Populating questions for exam instance taking too long.")
+                        .build(),
+                HttpStatus.REQUEST_TIMEOUT);
+    }
+    @Override
+    @CircuitBreaker(name = "retrieveScore", fallbackMethod = "retrieveScoreFallbackMethod")
+    @Retry(name = "retrieveScore", fallbackMethod = "retrieveScoreFallbackMethod")
     public Integer calculateScoreForExamSession(Long examId, List<AnswerResponseDto> answerResponseDto) {
         ExamEntity examEntity = examRepository.findById(examId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-       return feignClient.getScore(answerResponseDto).getBody();
+        return feignClient.getScore(answerResponseDto).getBody();
+    }
+
+    @Override
+    public ResponseEntity<ResponseDto> retrieveScoreFallbackMethod(Long examId, List<AnswerResponseDto> answerResponseDto, Throwable throwable) {
+        return new ResponseEntity<>(
+                ResponseDto.builder()
+                        .responseCode("")
+                        .responseMessage("Oops! Populating questions for exam instance taking too long.")
+                        .build(),
+                HttpStatus.REQUEST_TIMEOUT);
     }
 
     @Override
     public List<ExamResponseDto> fetchExamSession() {
-       return examRepository.findAll().stream().map(examEntity -> ExamResponseDto.builder()
-               .id(examEntity.getId())
-               .title(examEntity.getTitle())
-               .listOfQuestions(feignClient.getQuestionsBasedOnId(examEntity.getQuestionId()).getBody())
-               .build()).collect(Collectors.toList());
+        return examRepository.findAll().stream().map(examEntity -> ExamResponseDto.builder()
+                .id(examEntity.getId())
+                .title(examEntity.getTitle())
+                .listOfQuestions(feignClient.getQuestionsBasedOnId(examEntity.getQuestionId()).getBody())
+                .build()).collect(Collectors.toList());
     }
 }
